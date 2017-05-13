1a80eac250383cc0f3bdcbf82beb7376e6ea12f2
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-05-13 20:10:26 -0300

    Agregué los inserts de los tipos de usuarios y los usuarios (administrativo, profesor, alumno y administrador).

--- base de datos/base_de_datos.sql
+++ base de datos/base_de_datos.sql
@@ -1,86 +1,141 @@
--- -----------------------------------------------------
--- Table `encuestas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `encuestas` (
-  `id_encuesta` INT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NOT NULL,
-  `id_curso` INT(11) NOT NULL,
-  `fecha_inicio` DATETIME NULL,
-  `fecha_fin` DATETIME NULL,
-  PRIMARY KEY (`id_encuesta`));
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 13-05-2017 a las 20:09:38
+-- Versión del servidor: 5.1.68-community
+-- Versión de PHP: 5.2.5
 
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Table `preguntas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `preguntas` (
-  `id_pregunta` INT NOT NULL AUTO_INCREMENT,
-  `id_encuesta` INT NOT NULL,
-  `descripcion` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_pregunta`),
-  INDEX `fk_preguntas_encuestas_idx` (`id_encuesta` ASC));
+--
+-- Base de datos: `facultad_parcial2`
+--
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `respuestas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `respuestas` (
-  `id_respuesta` INT NOT NULL AUTO_INCREMENT,
-  `id_pregunta` INT NOT NULL,
-  `descripcion` VARCHAR(255) NOT NULL,
-  `tipo` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_respuesta`),
-  INDEX `fk_respuestas_preguntas1_idx` (`id_pregunta` ASC));
+--
+-- Estructura de tabla para la tabla `comisiones`
+--
 
-
--- -----------------------------------------------------
--- Table `tipos_usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tipos_usuarios` (
-  `id_tipo` INT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_tipo`));
-
-
--- -----------------------------------------------------
--- Table `usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `id_tipo` INT NOT NULL,
-  `nombre` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  INDEX `fk_usuarios_tipos_usuarios1_idx` (`id_tipo` ASC));
-
-
--- -----------------------------------------------------
--- Table `comisiones`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comisiones` (
-  `id_comision` INT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_comision`));
+  `id_comision` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_comision`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `cursos`
--- -----------------------------------------------------
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
 CREATE TABLE IF NOT EXISTS `cursos` (
-  `id_curso` INT NOT NULL AUTO_INCREMENT,
-  `id_comision` INT NOT NULL,
-  `id_usuario` INT NOT NULL,
-  `descripcion` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_curso`),
-  INDEX `fk_cursos_comisiones1_idx` (`id_comision` ASC),
-  INDEX `fk_cursos_usuarios1_idx` (`id_usuario` ASC));
+  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+  `id_comision` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `descripcion` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id_curso`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `encuestas_has_usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `encuestas_has_usuarios` (
-  `encuestas_id_encuesta` INT NOT NULL,
-  `usuarios_id_usuario` INT NOT NULL,
-  PRIMARY KEY (`encuestas_id_encuesta`, `usuarios_id_usuario`),
-  INDEX `fk_encuestas_has_usuarios_usuarios1_idx` (`usuarios_id_usuario` ASC),
-  INDEX `fk_encuestas_has_usuarios_encuestas1_idx` (`encuestas_id_encuesta` ASC));
+--
+-- Estructura de tabla para la tabla `encuestas`
+--
+
+CREATE TABLE IF NOT EXISTS `encuestas` (
+  `id_encuesta` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `fecha_inicio` datetime DEFAULT NULL,
+  `fecha_fin` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_encuesta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuestas_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `encuestas_usuarios` (
+  `encuestas_id_encuesta` int(11) NOT NULL,
+  `usuarios_id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`encuestas_id_encuesta`,`usuarios_id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `id_pregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `id_encuesta` int(11) NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_pregunta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE IF NOT EXISTS `respuestas` (
+  `id_respuesta` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pregunta` int(11) NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `tipo` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_respuesta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `tipos_usuarios` (
+  `id_tipo` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_tipo`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `tipos_usuarios`
+--
+
+INSERT INTO `tipos_usuarios` (`id_tipo`, `descripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Administrativo'),
+(3, 'Alumno'),
+(4, 'Profesor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo` int(11) NOT NULL,
+  `nombre` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `fk_usuarios_tipos_usuarios1_idx` (`id_tipo`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `id_tipo`, `nombre`) VALUES
+(1, 1, 'Administrador'),
+(2, 2, 'Administrativo'),
+(3, 3, 'Alumno'),
+(4, 4, 'Profesor');
