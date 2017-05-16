a31423416907af1fa398c1f3fbb2ed33b75d5b7f
skydarkness47 <yonathanaguirre@live.com>
2017-05-16 19:00:58 -0300

    Agregado estilos de login y validaciones
    Agregado estilos de login y validaciones

--- proyecto/src/pages/login/login.ts
+++ proyecto/src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {Usuario} from "../../pages/grilla/usuario";
+import {Usuario} from "../pages/usuario/usuario";
 import {Http} from '@angular/http';
 import 'rxjs/Rx'; 
 
@@ -9,11 +9,14 @@ import 'rxjs/Rx';
   templateUrl: 'login.html'
 })
 
-
 export class Login {
 
 listaBD = {};
 
+   Login = {
+ usuario: "",
+  clave: ""
+   } 
   constructor(public navCtrl: NavController,public http: Http) {
     this.http.get("http://localhost/ws1/usuarios")
   .map(res => res.json())
