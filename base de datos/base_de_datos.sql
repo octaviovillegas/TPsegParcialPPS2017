440aab2088a6a746c8d65504279468b9cd0c5e64
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-05-20 09:47:04 -0300

    actualice ignore de github

--- base de datos/base_de_datos.sql
+++ base de datos/base_de_datos.sql
@@ -4,7 +4,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 19-05-2017 a las 00:04:48
+-- Tiempo de generación: 20-05-2017 a las 12:43:24
 -- Versión del servidor: 10.1.22-MariaDB
 -- Versión de PHP: 5.2.17
 
@@ -20,6 +20,108 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `u745144718_bddtp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comisiones`
+--
+
+CREATE TABLE IF NOT EXISTS `comisiones` (
+  `id_comision` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_comision`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+  `id_comision` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `descripcion` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  PRIMARY KEY (`id_curso`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuestas`
+--
+
+CREATE TABLE IF NOT EXISTS `encuestas` (
+  `id_encuesta` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `fecha_inicio` datetime DEFAULT NULL,
+  `fecha_fin` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_encuesta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuestas_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `encuestas_usuarios` (
+  `encuestas_id_encuesta` int(11) NOT NULL,
+  `usuarios_id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`encuestas_id_encuesta`,`usuarios_id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `id_pregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `id_encuesta` int(11) NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_pregunta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE IF NOT EXISTS `respuestas` (
+  `id_respuesta` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pregunta` int(11) NOT NULL,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `tipo` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_respuesta`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `tipos_usuarios` (
+  `id_tipo` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_tipo`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `tipos_usuarios`
+--
+
+INSERT INTO `tipos_usuarios` (`id_tipo`, `descripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Administrativo'),
+(3, 'Alumno'),
+(4, 'Profesor');
 
 -- --------------------------------------------------------
 
