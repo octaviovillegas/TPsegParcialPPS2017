e0c32863ca2ba58b54b5f3305153e900d5ac4073
skydarkness47 <yonathanaguirre@live.com>
2017-05-28 12:34:01 -0300

    alta de encuestas y preguntas funcionando

--- proyecto/src/pages/Encuestas/encuestas.ts
+++ proyecto/src/pages/Encuestas/encuestas.ts
@@ -12,44 +12,49 @@ import {Http} from '@angular/http';
 })
 export class Encuestas {
     private usuarioLogueado;
-        private queHago = {
-          tipo:"",
-          pregunta:""
+        private datos = {
+          queHago:"",
+          idEncuesta:Number
         };
-        private pregunta;
 private listaPreguntas: Array<string>;
 private listaTipo: Array<string>;
 private listaEncuestas;
 private listaCursos;
 private cursoSeleccionado;
+private encuestaSeleccionada;
 
  constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController
-,public NavParams: NavParams,private http: Http)
+,public NavParams: NavParams,private http: Http,public af: AngularFire)
   {
-  this.usuarioLogueado = NavParams;
   this.http.get("http://tppps2.hol.es/ws1/cursos")
   .map(res => res.json())
   .subscribe((quote) =>{
     this.listaCursos = quote;
     console.info(this.listaCursos);
   });
+
+    this.usuarioLogueado = NavParams;
+  this.http.get("http://tppps2.hol.es/ws1/encuestas")
+  .map(res => res.json())
+  .subscribe((quote) =>{
+    this.listaEncuestas = quote;
+    console.info(this.listaEncuestas);
+  });
+  
   }
-    agregarTextbox(){
-      this.queHago.tipo = "text";
-        let modal = this.modalCtrl.create(Modales,this.queHago);
-        modal.present();
+    agregarPregunta(datos){
+      console.log(datos);
+            this.datos.idEncuesta=datos;
+      this.datos.queHago="AgregarPregunta";
+       let modal = this.modalCtrl.create(Modales,this.datos);
+      modal.present(); 
     }
     
-     agregarSelect(){
-      this.queHago.tipo = "select";
-      console.log(this.queHago);
-        let modal = this.modalCtrl.create(Modales,this.queHago);
-        modal.present();
-     console.info(modal);
-    }
-    public setPregunta(){
-      
-    }
+   nuevaEncuesta(){
+      this.datos.queHago="AgregarEncuesta";
+       let modal = this.modalCtrl.create(Modales,this.datos);
+      modal.present(); 
+   }
 
     cursoSelecionado(){
       
--- proyecto/src/pages/encuestas/modales/modales.ts
+++ proyecto/src/pages/encuestas/modales/modales.ts
@@ -4,36 +4,94 @@ import { NavController,NavParams } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import { ModalController, ViewController } from 'ionic-angular';
 import {Encuestas} from "../encuestas";
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'page-Modales',
   templateUrl: 'modales.html'
 })
+
 export class Modales {
-        public queHacer={
-          tipo:"",
-          pregunta:""
+        private datos = {
+          queHago:"",
+          idEncuesta:Number
         };
-        private veoText=false;
-        private encuestas;
+        private encuesta={
+          descripcion:"",
+          fechaInicio:"",
+          fechaFin:""
+           }
+        private veoPreguntas=false;
+        private encuestas :FirebaseListObservable<any[]>;
+        private idEncuesta;
+        private tipoPregunta;
+        private veoEncuesta=false;
+        private opcion1;
+        private opcion2;
+        private opcion3;
+        private opcion4;
+        private pregunta;
  constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController
- ,public NavParams: NavParams) 
+ ,public NavParams: NavParams,public af: AngularFire,private http: Http) 
 {
-  
-   this.queHacer = NavParams.data;
-   console.info(this.queHacer);
-        if(this.queHacer.tipo == "text"){
-          this.veoText=true; 
+   this.datos = NavParams.data;
+   this.idEncuesta=this.datos.idEncuesta;
+   console.info(this.datos);
+        if(this.datos.queHago == "AgregarPregunta"){
+          this.veoPreguntas=true; 
+             }else if(this.datos.queHago =="AgregarEncuesta"){
+  this.veoEncuesta =true;
+
              }
     }
      
 
 
 dismiss() {
-   this.viewCtrl.dismiss(this.queHacer).then(() =>{
-  
-   });
+  this.veoEncuesta =false;
+  this.veoPreguntas=false;
+   this.viewCtrl.dismiss();
  }
+
+altaEncuesta(){
+  console.info(this.encuesta);
+    this.http.post("http://tppps2.hol.es/ws1/encuestas/alta", {
+     descripcion:this.encuesta.descripcion,
+     fechaInicio:this.encuesta.fechaInicio,
+     fechaFin:this.encuesta.fechaFin
+     })
+    .map(res => res.json())
+    .subscribe((quote) =>{
+      console.info(quote);
+     });
+       
+      this.viewCtrl.dismiss();
+}
+altaPregunta(){
+  console.log(this.idEncuesta);
+  if(this.tipoPregunta =="1"){
+    this.tipoPregunta ="text";
+  }else if(this.tipoPregunta =="2"){
+    this.tipoPregunta ="radio";
+  }else if(this.tipoPregunta =="3"){
+    this.tipoPregunta ="checkbox";
+  }
+    this.http.post("http://tppps2.hol.es/ws1/pregunta/alta", {
+    idEncuesta:this.idEncuesta,
+    tipo:this.tipoPregunta,
+    pregunta:this.pregunta,
+    opcion1:this.opcion1,
+    opcion2:this.opcion2,
+    opcion3:this.opcion3,
+    opcion4:this.opcion4
+     })
+    .map(res => res.json())
+    .subscribe((quote) =>{
+      console.info(quote);
+     });
+       
+      this.viewCtrl.dismiss();
+}
 
 
 }
