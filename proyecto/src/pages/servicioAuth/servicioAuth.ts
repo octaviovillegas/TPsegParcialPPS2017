b1f6d671182193ad208ad54970db851246744f58
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-06-03 10:40:00 -0300

    Arregle el Login y administrador.ts

--- proyecto/src/pages/servicioAuth/servicioAuth.ts
+++ proyecto/src/pages/servicioAuth/servicioAuth.ts
@@ -11,54 +11,58 @@ import 'rxjs/Rx';
 
 export class servicioAuth {
 
-  currentUser: User;
-ListaUsuarios = "";
+    currentUser: User;
+    ListaUsuarios = "";
 
-constructor(private http: Http) {
-    this.http.get("http://tppps2.hol.es/ws1/usuarios")
-    //this.http.get("http://localhost/facultad/ws1/usuarios")
-  .map(res => res.json())
-  .subscribe((quote) =>{
-    this.ListaUsuarios = quote;
-    console.info(quote);
-  });
-}
-
-
-  public login(credenciales) {
-    console.info(credenciales);
-   let access =false;
-   let tipo ="";
-    if (credenciales.usuario === null || credenciales.clave === null) {
-      return Observable.throw("Por favor inserte las credenciales");
-    } else {
-      return Observable.create(observer => {
-         for (let entry of this.ListaUsuarios) {
-           console.info(entry);
-          if(credenciales.usuario === entry["usuario"] && credenciales.clave === entry["clave"] ){
-             access = true;
-             tipo = entry["tipo_usuario"];
-                    credenciales.id_usuario = entry['id_usuario'];
-          }
-                }
-            this.currentUser = new User(credenciales.id_usuario, credenciales.usuario, credenciales.clave,tipo);
-        observer.next(access);
-        observer.complete();
-      });
+    constructor(private http: Http) {
     }
-  }
 
+    public getUsuarioByUsuarioYClave (usuario, clave) {
 
+        return this.http.get('http://tppps2.hol.es/ws1/usuarios?usuario='+usuario+'&clave='+clave)
+        .map(r => r.json());
 
-  public getUserInfo() : User {
-    return this.currentUser;
-  }
+    }
 
-  public logout() {
-    return Observable.create(observer => {
-      this.currentUser = null;
-      observer.next(true);
-      observer.complete();
-    });
-  }
+    public login(credenciales) {
+
+        if (credenciales.usuario == null || credenciales.clave == null) {
+
+            return Observable.throw("Por favor inserte las credenciales");
+
+        } else {
+
+            return Observable.create(observer => {
+
+                this.getUsuarioByUsuarioYClave(credenciales.usuario, credenciales.clave).subscribe((usuario) => {
+
+                    // Si existe el usuario en la base de datos SQL entro.
+                    if (usuario) {
+                        this.currentUser = new User(usuario.id_usuario, usuario.usuario, usuario.clave, usuario.tipo_usuario);
+                        observer.next(true);
+                    } else {
+                        observer.next(false);
+                    }
+
+                    observer.complete();
+
+                });
+
+            });
+
+        }
+    }
+
+    public getUserInfo() : User {
+        return this.currentUser;
+    }
+
+    public logout() {
+        return Observable.create(observer => {
+            this.currentUser = null;
+            observer.next(true);
+            observer.complete();
+        });
+    }
+
 }
