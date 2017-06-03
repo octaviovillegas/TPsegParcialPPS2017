9a396860d22a2fdd0f5f0789f9c15e5ab57ee93b
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-06-03 11:53:54 -0300

    Agregue la modificacion/alta de usuarios en firebase.

--- proyecto/src/pages/grillas/alta-modal/alta-modal.ts
+++ proyecto/src/pages/grillas/alta-modal/alta-modal.ts
@@ -1,59 +1,86 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, NavOptions, ViewController} from 'ionic-angular';
-import {Http, URLSearchParams} from '@angular/http';
+import { IonicPage, NavController, NavParams, NavOptions, ViewController, AlertController } from 'ionic-angular';
+import { Http, URLSearchParams } from '@angular/http';
+import { AuthData } from '../../../providers/auth-data';
 /**
- * Generated class for the ModificacionModal page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+* Generated class for the ModificacionModal page.
+*
+* See http://ionicframework.com/docs/components/#navigation for more info
+* on Ionic pages and navigation.
+*/
 @Component({
-  selector: 'page-alta-modal',
-  templateUrl: 'alta-modal.html',
+    selector: 'page-alta-modal',
+    templateUrl: 'alta-modal.html',
 })
-export class AltaModal 
+export class AltaModal
 {
 
-      n;
-      t;
-      c;
-      u;
-      id_usuario;
-      id_tip;
+    n;
+    t;
+    c;
+    u;
+    id_usuario;
+    id_tip;
 
-      constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http, public viewCtrl: ViewController) 
-      {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http,
+        public viewCtrl: ViewController, public auth: AuthData, private alertCtrl: AlertController)
+    {
         this.t = navParams.data['tipo'];
-      }
+    }
 
-      Alta(nombre, usuario, clave, id_tipo)
-      {
+    Alta(nombre, usuario, clave, id_tipo)
+    {
 
         if(nombre == null || nombre=="" ||usuario==null || usuario=="" || clave== null || clave=="" || id_tipo==null || id_tipo=="" )
         {
-          alert ("Debe completar todos los campos!");
+            alert ("Debe completar todos los campos!");
         }
         else{
-          this.http.post("http://tppps2.hol.es/ws1/usuarios/alta", {
-              clave: this.c,
-              nombre: this.n,
-              usuario: this.u,
-              id_tipo: this.t
-          })
-          .map(res => res.json())
-          .subscribe((quote) =>{
-            console.info(quote);
-            this.viewCtrl.dismiss();
-          });
-            
+
+            // Creo el usuario en firebase`
+            this.auth.signupUser(this.u, this.c).then((success) => {
+
+                if (success) {
+
+                    // Creo el usuario en la base de datos sql.
+                    this.http.post("http://tppps2.hol.es/ws1/usuarios/alta", {
+                        clave: this.c,
+                        nombre: this.n,
+                        usuario: this.u,
+                        id_tipo: this.t
+                    })
+                    .map(res => res.json())
+                    .subscribe((quote) =>{
+                        console.info(quote);
+                        this.viewCtrl.dismiss();
+                    });
+
+                } else {
+                    this.showMsg('No se pudo crear el usuario.');
+                }
+
+            }).catch(e => {
+                console.log(e);
+                this.showMsg(e);
+            });
+
         }
-      
-      }
-      
-      cancelar()
-      {
-          this.viewCtrl.dismiss();
-      }
+
+    }
+
+    showMsg(text) {
+        let alert = this.alertCtrl.create({
+            title: 'Error al crear el usuario',
+            subTitle: text,
+            buttons: ['OK']
+        });
+        alert.present(prompt);
+    }
+
+    cancelar()
+    {
+        this.viewCtrl.dismiss();
+    }
 
 
-  }
+}
