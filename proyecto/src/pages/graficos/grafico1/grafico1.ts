e4aecbdd84898ca039cf4ea74bc6deea582af7d4
Aixa <aixacasanova@gmail.com>
2017-06-08 21:27:42 -0300

    se cambia charts por hight-chart
    se cambia charts por hight-chart

--- proyecto/src/pages/graficos/grafico1/grafico1.ts
+++ proyecto/src/pages/graficos/grafico1/grafico1.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController  } from 'ionic-angular';
 import {Http} from '@angular/http';
-
-/**
+import Highcharts from 'highcharts'
+/** 
  * Generated class for the ModificacionModal page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
@@ -16,55 +16,34 @@ export class grafico1
 {
 
 
+	constructor(public navCtrl: NavController,public http:Http) {
+		this.traerdatos();
+	
+	}
 
-contResp;
-total: any[] = [];
-graf1;
-
-
-chartOptions: any = {
-    scaleShowVerticalLines: false,
-    responsive: true,
-     scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    }
-  };
- 
- 
-  chartLabels: string[];
-  chartType: string = 'bar';
-  chartLegend: boolean = true;
-
-
-  chartData: any[] ;
-  
-  
-  pregs=[];
-  preganterior=null;
-  Unapregunta;
-  conjAver=[];
-      constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http, public viewCtrl: ViewController) 
-      {
-        this.traerGraficos(); 
-
-      }
-
-
-    traerGraficos()
-    {
-      let pregunta=null; 
-      
-           this.http.get("http://tppps2.hol.es/ws1/GrafRespuestasDePreguntas")
+	contResp;
+	total: any[] = [];
+	graf1;
+	pregs=[];
+	preganterior=null;
+	Unapregunta;
+	conjAver=[];
+  conjAver2=[];
+	traerdatos()
+	{
+		 
+		  this.http.get("http://tppps2.hol.es/ws1/GrafRespuestasDePreguntas")
             .map(res => res.json())
             .subscribe((quote) =>{
             this.graf1 = quote;   
-             
+            
              for(let g of this.graf1)
              {
+				  
+				  let num=g['cantResp']; 
+				  
+				    this.conjAver.push({name:g['opcion'], y:Number.parseInt(num), drilldown:g['opcion'],preg:g['descripcion']});
+				
                if(this.preganterior != g['descripcion'])
                {
                  this.preganterior=g['descripcion'];
@@ -72,39 +51,67 @@ chartOptions: any = {
                 
                }
              }
-           
+		 
+		 
            });
-    
-    }
+	}
 
-    onChange(event)
+	onChange(event)
+	{
+    this.conjAver2=[];
+		for (let obj of this.conjAver) 
     {
-          this.conjAver=[];
-          console.info(event);
-
-          for(let g of this.graf1)
-          {
-            if(g.descripcion==event)
-            {
-              this.conjAver.push(g);
-            }
-          }
-      
-      
-            for (let pr of this.conjAver)
-             {
-               let num:number=pr['cantResp'];
-                  this.total.push({label:pr['opcion'], data:1});
-            }
-                  console.info(JSON.stringify(this.total));
-                  this.chartLabels = event;
-                  this.chartData=this.total;
-                  console.info(this.chartData);
+        console.info(obj);
+        console.info(event);
+        if(obj['preg']==event)
+        {
+          this.conjAver2.push(obj);
+        }
+        console.info(this.conjAver2);
     }
- 
+    
+			Highcharts.chart('container', {
+				chart: {
+					type: 'column'
+				},
+				title: {
+					text: 'Preguntas por respuesta'
+				},
+				xAxis: {
+					type: 'category'
+				},
+				yAxis: {
+					title: {
+						text: 'cantidad'
+					}
 
- 
+				},
+				legend: {
+					enabled: false
+				},
+				plotOptions: {
+					series: {
+						borderWidth: 0,
+						dataLabels: {
+							enabled: true,
+							format: '{point.y:1f}'
+						}
+					}
+				},
+
+				tooltip: {
+					headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+					pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
+				},
+
+				series: [{
+					name: 'Brands',
+					colorByPoint: true,
+					data: this.conjAver2
+				}]
+			});
+
+	} 
 
 
- 
 }
