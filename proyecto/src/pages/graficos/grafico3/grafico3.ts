53a8c038747e51803629359d07e4beec2d26db35
Aixa <aixacasanova@gmail.com>
2017-06-09 13:48:15 -0300

    se agrega grafico cantidad de alumnos por curso
    se agrega grafico cantidad de alumnos por curso

--- proyecto/src/pages/graficos/grafico3/grafico3.ts
+++ proyecto/src/pages/graficos/grafico3/grafico3.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, NavOptions, ViewController} from 'ionic-angular';
-import {Http, URLSearchParams} from '@angular/http';
-/**
+import { IonicPage, NavController, NavParams, ViewController, AlertController  } from 'ionic-angular';
+import {Http} from '@angular/http';
+import Highcharts from 'highcharts'
+/** 
  * Generated class for the ModificacionModal page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
@@ -11,15 +12,112 @@ import {Http, URLSearchParams} from '@angular/http';
   selector: 'page-grafico3',
   templateUrl: 'grafico3.html',
 })
-export class grafico3 
+export class grafico3
 {
 
-     
-      constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http, public viewCtrl: ViewController) 
-      {
-       
-      }
- 
+
+	constructor(public navCtrl: NavController,public http:Http) {
+		this.traerdatos();
+	
+	}
+
+	contResp;
+	total: any[] = [];
+	graf1;
+	cursos=[];
+	cursoanterior=null;
+	Uncurso;
+	conjAver=[];
+  conjAver2=[];
+	traerdatos()
+	{
+		 
+		  this.http.get("http://tppps2.hol.es/ws1/GrafAlumnosPorCurso")
+            .map(res => res.json())
+            .subscribe((quote) =>{
+            this.graf1 = quote;   
+            console.info(this.graf1);
+             for(let g of this.graf1)
+             {
+				  
+				  let num=g['cantidad_alumnos']; 
+				  
+				    this.conjAver.push({name:g['curso'], y:Number.parseInt(num), drilldown:g['curso']});
+				
+               if(this.cursoanterior != g['curso'])
+               {
+                 this.cursoanterior=g['curso'];
+                 this.cursos.push(this.cursoanterior);
+                
+               }
+             }
+		 
+		 
+           });
+           this.cursos.push("Todos");
+           console.info(this.cursos);
+	}
+
+	onChange(event)
+	{
+    this.conjAver2=[];
+    if(event=="Todos")
+    {this.conjAver2=this.conjAver}
+    else{
+        for (let obj of this.conjAver) 
+        {
+            console.info(obj);
+            console.info(event);
+            if(obj['name']==event)
+            {
+              this.conjAver2.push(obj);
+            }
+            console.info(this.conjAver2);
+        }
+    }
+    
+			Highcharts.chart('container', {
+				chart: {
+					type: 'column'
+				},
+				title: {
+					text: "Curso: "+event
+				},
+				xAxis: {
+					type: 'category'
+				},
+				yAxis: {
+					title: {
+						text: 'cantidad de alumnos'
+					}
+
+				},
+				legend: {
+					enabled: false
+				},
+				plotOptions: {
+					series: {
+						borderWidth: 0,
+						dataLabels: {
+							enabled: true,
+							format: '{point.y:1f}'
+						}
+					}
+				},
+
+				tooltip: {
+					headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+					pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
+				},
+
+				series: [{
+					name: 'Brands',
+					colorByPoint: true,
+					data: this.conjAver2
+				}]
+			});
+
+	} 
 
 
-  }
+}
