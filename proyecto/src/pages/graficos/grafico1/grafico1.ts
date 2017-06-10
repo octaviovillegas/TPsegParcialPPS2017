3aa0bf3a5437b3422d1520360b079d88fcdaa5aa
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-06-10 11:01:42 -0300

    Agregué spinners al Grafico1, Grafico2 y Grafico3.

--- proyecto/src/pages/graficos/grafico1/grafico1.ts
+++ proyecto/src/pages/graficos/grafico1/grafico1.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController  } from 'ionic-angular';
 import {Http} from '@angular/http';
 import Highcharts from 'highcharts'
-/** 
+/**
  * Generated class for the ModificacionModal page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
@@ -12,13 +12,14 @@ import Highcharts from 'highcharts'
   selector: 'page-grafico1',
   templateUrl: 'grafico1.html',
 })
-export class Grafico1 
+export class Grafico1
 {
 
+    cargando = false;
 
 	constructor(public navCtrl: NavController,public http:Http) {
 		this.traerdatos();
-	
+
 	}
 
 	contResp;
@@ -29,90 +30,95 @@ export class Grafico1
 	Unapregunta;
 	conjAver=[];
   conjAver2=[];
-	cargando:boolean=true;
-	traerdatos()
-	{
-		 this.cargando=true;
-		  this.http.get("http://tppps2.hol.es/ws1/GrafRespuestasDePreguntas")
-            .map(res => res.json())
-            .subscribe((quote) =>{
-            this.graf1 = quote;   
-            
-             for(let g of this.graf1)
-             {
-				  
-				  let num=g['cantResp']; 
-				  
-				    this.conjAver.push({name:g['opcion'], y:Number.parseInt(num), drilldown:g['opcion'],preg:g['descripcion']});
-				
-               if(this.preganterior != g['descripcion'])
-               {
-                 this.preganterior=g['descripcion'];
-                 this.pregs.push(this.preganterior);
-                
-               }
-             }
-		 
-						 this.cargando=false;
-           });
-	}
+  traerdatos()
+  {
+      this.cargando = true;
+      this.http.get("http://tppps2.hol.es/ws1/GrafRespuestasDePreguntas")
+      .map(res => res.json())
+      .subscribe((quote) => {
+          this.cargando = false;
+          this.graf1 = quote;
+
+          for(let g of this.graf1)
+          {
+
+              let num=g['cantResp'];
+
+              this.conjAver.push({name:g['opcion'], y:Number.parseInt(num), drilldown:g['opcion'],preg:g['descripcion']});
+
+              if(this.preganterior != g['descripcion'])
+              {
+                  this.preganterior=g['descripcion'];
+                  this.pregs.push(this.preganterior);
+
+              }
+          }
+
+
+      }, error => {
+          this.cargando = false;
+      });
+  }
 
 	onChange(event)
 	{
-    this.conjAver2=[];
-		for (let obj of this.conjAver) 
-    {
-        console.info(obj);
-        console.info(event);
-        if(obj['preg']==event)
+        this.cargando = true;
+        this.conjAver2=[];
+        for (let obj of this.conjAver)
         {
-          this.conjAver2.push(obj);
+            console.info(obj);
+            console.info(event);
+            if(obj['preg']==event)
+            {
+                this.conjAver2.push(obj);
+            }
+            console.info(this.conjAver2);
         }
-        console.info(this.conjAver2);
-    }
-    
-			Highcharts.chart('container', {
-				chart: {
-					type: 'column'
-				},
-				title: {
-					text: event
-				},
-				xAxis: {
-					type: 'category'
-				},
-				yAxis: {
-					title: {
-						text: 'cantidad'
-					}
 
-				},
-				legend: {
-					enabled: false
-				},
-				plotOptions: {
-					series: {
-						borderWidth: 0,
-						dataLabels: {
-							enabled: true,
-							format: '{point.y:1f}'
-						}
-					}
-				},
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: event
+            },
+            xAxis: {
+                type: 'category'
+            },
+            yAxis: {
+                title: {
+                    text: 'cantidad'
+                }
 
-				tooltip: {
-					headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-					pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
-				},
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    borderWidth: 0,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.y:1f}'
+                    }
+                }
+            },
 
-				series: [{
-					name: 'Brands',
-					colorByPoint: true,
-					data: this.conjAver2
-				}]
-			});
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
+            },
 
-	} 
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: this.conjAver2
+            }]
+        });
+
+        this.cargando = false;
+
+	}
 
 
 }
--- proyecto/src/pages/graficos/grafico3/grafico3.ts
+++ proyecto/src/pages/graficos/grafico3/grafico3.ts
@@ -2,122 +2,126 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController  } from 'ionic-angular';
 import {Http} from '@angular/http';
 import Highcharts from 'highcharts'
-/** 
- * Generated class for the ModificacionModal page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+/**
+* Generated class for the ModificacionModal page.
+*
+* See http://ionicframework.com/docs/components/#navigation for more info
+* on Ionic pages and navigation.
+*/
 @Component({
-  selector: 'page-grafico3',
-  templateUrl: 'grafico3.html',
+    selector: 'page-grafico3',
+    templateUrl: 'grafico3.html',
 })
 export class Grafico3
 {
 
+    cargando = false;
 
-	constructor(public navCtrl: NavController,public http:Http) {
-		this.traerdatos();
-	
-	}
+    constructor(public navCtrl: NavController,public http:Http) {
+        this.traerdatos();
 
-	contResp;
-	total: any[] = [];
-	graf1;
-	cursos=[];
-	cursoanterior=null;
-	Uncurso;
-	conjAver=[];
-  conjAver2=[];
-	traerdatos()
-	{
-		 
-		  this.http.get("http://tppps2.hol.es/ws1/GrafAlumnosPorCurso")
-            .map(res => res.json())
-            .subscribe((quote) =>{
-            this.graf1 = quote;   
-            console.info(this.graf1);
-             for(let g of this.graf1)
-             {
-				  
-				  let num=g['cantidad_alumnos']; 
-				  
-				    this.conjAver.push({name:g['curso'], y:Number.parseInt(num), drilldown:g['curso']});
-				
-               if(this.cursoanterior != g['curso'])
-               {
-                 this.cursoanterior=g['curso'];
-                 this.cursos.push(this.cursoanterior);
-                
-               }
-             }
-		 
-		 
-           });
-           this.cursos.push("Todos");
-           console.info(this.cursos);
-	}
-
-	onChange(event)
-	{
-    this.conjAver2=[];
-    if(event=="Todos")
-    {this.conjAver2=this.conjAver}
-    else{
-        for (let obj of this.conjAver) 
-        {
-            console.info(obj);
-            console.info(event);
-            if(obj['name']==event)
-            {
-              this.conjAver2.push(obj);
-            }
-            console.info(this.conjAver2);
-        }
     }
-    
-			Highcharts.chart('container', {
-				chart: {
-					type: 'column'
-				},
-				title: {
-					text: "Curso: "+event
-				},
-				xAxis: {
-					type: 'category'
-				},
-				yAxis: {
-					title: {
-						text: 'cantidad de alumnos'
-					}
 
-				},
-				legend: {
-					enabled: false
-				},
-				plotOptions: {
-					series: {
-						borderWidth: 0,
-						dataLabels: {
-							enabled: true,
-							format: '{point.y:1f}'
-						}
-					}
-				},
+    contResp;
+    total: any[] = [];
+    graf1;
+    cursos=[];
+    cursoanterior=null;
+    Uncurso;
+    conjAver=[];
+    conjAver2=[];
+    traerdatos()
+    {
+        this.cargando = true;
+        this.http.get("http://tppps2.hol.es/ws1/GrafAlumnosPorCurso")
+        .map(res => res.json())
+        .subscribe((quote) => {
+            this.cargando = false;
+            this.graf1 = quote;
+            console.info(this.graf1);
+            for(let g of this.graf1)
+            {
 
-				tooltip: {
-					headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-					pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
-				},
+                let num=g['cantidad_alumnos'];
 
-				series: [{
-					name: 'Brands',
-					colorByPoint: true,
-					data: this.conjAver2
-				}]
-			});
+                this.conjAver.push({name:g['curso'], y:Number.parseInt(num), drilldown:g['curso']});
 
-	} 
+                if(this.cursoanterior != g['curso'])
+                {
+                    this.cursoanterior=g['curso'];
+                    this.cursos.push(this.cursoanterior);
+
+                }
+            }
+
+
+        }, error => {
+            this.cargando = false;
+        });
+        this.cursos.push("Todos");
+        console.info(this.cursos);
+    }
+
+    onChange(event)
+    {
+        this.conjAver2=[];
+        if(event=="Todos")
+        {this.conjAver2=this.conjAver}
+        else{
+            for (let obj of this.conjAver)
+            {
+                console.info(obj);
+                console.info(event);
+                if(obj['name']==event)
+                {
+                    this.conjAver2.push(obj);
+                }
+                console.info(this.conjAver2);
+            }
+        }
+
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: "Curso: "+event
+            },
+            xAxis: {
+                type: 'category'
+            },
+            yAxis: {
+                title: {
+                    text: 'cantidad de alumnos'
+                }
+
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    borderWidth: 0,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.y:1f}'
+                    }
+                }
+            },
+
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:f}</b> of total<br/>'
+            },
+
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: this.conjAver2
+            }]
+        });
+
+    }
 
 
 }
