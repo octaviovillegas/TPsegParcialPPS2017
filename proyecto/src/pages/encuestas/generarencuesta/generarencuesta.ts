4d9c01e4180c969370f13a36a2b89ba9314e06af
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-06-22 18:07:01 -0300

    Corregí el generador de encuestas y la modal para alta y modificación. Les agregué spinners.

--- proyecto/src/pages/encuestas/generarencuesta/generarencuesta.ts
+++ proyecto/src/pages/encuestas/generarencuesta/generarencuesta.ts
@@ -1,89 +1,102 @@
 import { Component } from '@angular/core';
 import 'rxjs/Rx';
-import { NavController,NavParams } from 'ionic-angular';
-import { ModalController, ViewController } from 'ionic-angular';
+import { NavController,NavParams, ModalController, ViewController, ToastController } from 'ionic-angular';
 import {Modales} from "../../encuestas/modales/modales";
 import {Http} from '@angular/http';
 import { servicioAuth } from '../../servicioAuth/servicioAuth';
 
 @Component({
-  selector: 'page-generarencuesta',
-  templateUrl: 'generarencuesta.html'
+    selector: 'page-generarencuesta',
+    templateUrl: 'generarencuesta.html'
 })
 export class GenerarEncuesta {
+
     private usuarioLogueado;
-        private datos = {
-          queHago:"",
-          idEncuesta:Number
-        };
-private listaPreguntas: Array<string>;
-private listaTipo: Array<string>;
-private listaEncuestas;
-private listaCursos;
-private cursoSeleccionado;
-private encuestaSeleccionada;
+    private datos = {
+        queHago:"",
+        idEncuesta:Number
+    };
+
+    private listaPreguntas: Array<string>;
+    private listaEncuestas;
+
+    cargando = false;
 
 
- constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController
-,public NavParams: NavParams,private http: Http,servAuth:servicioAuth)
-  {
-this.usuarioLogueado=servAuth.getUserInfo();    
- 
- this.http.get("http://tppps2.hol.es/ws1/encuestas")
-  .map(res => res.json())
-  .subscribe((quote) =>{
-    this.listaEncuestas = quote;
-    console.info(this.listaEncuestas);
-  });
-  
-this.usuarioLogueado=servAuth.getUserInfo();
+    constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController
+        ,public NavParams: NavParams,private http: Http,servAuth:servicioAuth, public toastCtrl: ToastController)
+        {
+            this.usuarioLogueado = servAuth.getUserInfo();
+            this.cargarEncuestas();
+        }
 
-this.cargarEncuesta();
-
-  }
-    agregarPregunta(datos){
-      console.log(datos);
+        agregarPregunta(datos) {
+            console.log(datos);
             this.datos.idEncuesta=datos;
-      this.datos.queHago="AgregarPregunta";
-       let modal = this.modalCtrl.create(Modales,this.datos);
-      modal.present();
-    }
+            this.datos.queHago="AgregarPregunta";
+            let modal = this.modalCtrl.create(Modales,this.datos);
 
-    cargarEncuesta(){
-      this.http.get("http://tppps2.hol.es/ws1/encuestas")
-  .map(res => res.json())
-  .subscribe((quote) =>{
-    this.listaEncuestas = quote;
-    console.info(this.listaEncuestas);
-  });
-    }
-
-   nuevaEncuesta(){
-      this.datos.queHago="AgregarEncuesta";
-       let modal = this.modalCtrl.create(Modales,this.datos);
-
-        modal.onDidDismiss(data => {
-                   this.cargarEncuesta();
+            modal.onDidDismiss(data => {
+                console.log('onDidDismiss: ', data);
+                if (data) {
+                    this.cargarEncuestas();
+                    this.mostrarMensaje('Pregunta agregada con éxito!');
+                }
             });
 
-      modal.present(); 
+            modal.present();
+        }
+
+        cargarEncuestas() {
+            this.cargando = true;
+            this.http.get("http://tppps2.hol.es/ws1/encuestas")
+            .map(res => res.json())
+            .subscribe((quote) =>{
+                this.cargando = false;
+                this.listaEncuestas = quote;
+                console.info(this.listaEncuestas);
+            });
+        }
+
+        nuevaEncuesta(){
+            this.datos.queHago = "AgregarEncuesta";
+            let modal = this.modalCtrl.create(Modales, this.datos);
+
+            modal.onDidDismiss(data => {
+                console.log('onDidDismiss: ', data);
+                if (data) {
+                    this.cargarEncuestas();
+                    this.mostrarMensaje('Encuesta agregada con éxito!');
+                }
+            });
+
+            modal.present();
+
+        }
+
+        BorrarEncuesta(datos){
+            console.log(datos);
+            this.http.post("http://tppps2.hol.es/ws1/encuestas/borrar", {
+                idEncuesta:datos
+            })
+            .map(res => res.json())
+            .subscribe((quote) =>{
+                this.cargarEncuestas();
+                this.mostrarMensaje('Encuesta borrada con éxito!');
+                console.info(quote);
+            });
+
+        }
+
+        mostrarMensaje (mensaje) {
+            let toast = this.toastCtrl.create({
+                message: mensaje,
+                duration: 3000,
+                position: 'bottom'
+            });
+            toast.present();
+        }
 
 
-   }
-
-      BorrarEncuesta(datos){
-        console.log(datos);
-       this.http.post("http://tppps2.hol.es/ws1/encuestas/borrar", {
-     idEncuesta:datos
-         })
-    .map(res => res.json())
-    .subscribe((quote) =>{
-      this.cargarEncuesta();
-      console.info(quote);
-     });
 
     }
-
-
-
-}
--- proyecto/src/pages/encuestas/modales/modales.ts
+++ proyecto/src/pages/encuestas/modales/modales.ts
@@ -1,94 +1,121 @@
 import { Component } from '@angular/core';
 import 'rxjs/Rx';
-import { NavController,NavParams } from 'ionic-angular';
+import { NavController,NavParams, ToastController } from 'ionic-angular';
 import { ModalController, ViewController } from 'ionic-angular';
 import {Http} from '@angular/http';
 
 @Component({
-  selector: 'page-Modales',
-  templateUrl: 'modales.html'
+    selector: 'page-Modales',
+    templateUrl: 'modales.html'
 })
 
 export class Modales {
-        private datos = {
-          queHago:"",
-          idEncuesta:Number
-        };
-        private encuesta={
-          descripcion:"",
-          fechaInicio:"",
-          fechaFin:""
-           }
-        private veoPreguntas=false;
-        private idEncuesta;
-        private tipoPregunta;
-        private veoEncuesta=false;
-        private opcion1;
-        private opcion2;
-        private opcion3;
-        private opcion4;
-        private pregunta;
- constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController
- ,public NavParams: NavParams, private http: Http)
-{
-   this.datos = NavParams.data;
-   this.idEncuesta=this.datos.idEncuesta;
-   console.info(this.datos);
-        if(this.datos.queHago == "AgregarPregunta"){
-          this.veoPreguntas=true;
-             }else if(this.datos.queHago =="AgregarEncuesta"){
-  this.veoEncuesta =true;
+    private datos = {
+        queHago:"",
+        idEncuesta:Number
+    };
+    private encuesta={
+        descripcion:"",
+        fechaInicio:"",
+        fechaFin:""
+    }
+    private veoPreguntas=false;
+    private idEncuesta;
+    private tipoPregunta = '';
+    private veoEncuesta=false;
+    private opcion1;
+    private opcion2;
+    private opcion3;
+    private opcion4;
+    private pregunta = '';
 
-             }
+    cargando = false;
+
+    constructor(public navCtrl: NavController,public viewCtrl:ViewController,public modalCtrl: ModalController, public NavParams: NavParams, private http: Http, public toastCtrl: ToastController) {
+        this.datos = NavParams.data;
+        this.idEncuesta=this.datos.idEncuesta;
+        console.info(this.datos);
+
+        if(this.datos.queHago == "AgregarPregunta") {
+            this.veoPreguntas=true;
+        } else if(this.datos.queHago == "AgregarEncuesta") {
+            this.veoEncuesta =true;
+        }
     }
 
+    dismiss() {
 
+        this.veoEncuesta =false;
+        this.veoPreguntas=false;
+        this.viewCtrl.dismiss(false);
 
-dismiss() {
-  this.veoEncuesta =false;
-  this.veoPreguntas=false;
-   this.viewCtrl.dismiss(false);
- }
+    }
 
-altaEncuesta(){
-  console.info(this.encuesta);
-    this.http.post("http://tppps2.hol.es/ws1/encuestas/alta", {
-     descripcion:this.encuesta.descripcion,
-     fechaInicio:this.encuesta.fechaInicio,
-     fechaFin:this.encuesta.fechaFin
-     })
-    .map(res => res.json())
-    .subscribe((quote) =>{
-      console.info(quote);
-     });
+    altaEncuesta() {
 
-      this.viewCtrl.dismiss(true);
-}
-altaPregunta(){
-  console.log(this.idEncuesta);
-  if(this.tipoPregunta =="1"){
-    this.tipoPregunta ="text";
-  }else if(this.tipoPregunta =="2"){
-    this.tipoPregunta ="radio";
-  }else if(this.tipoPregunta =="3"){
-    this.tipoPregunta ="checkbox";
-  }
-    this.http.post("http://tppps2.hol.es/ws1/pregunta/alta", {
-    idEncuesta:this.idEncuesta,
-    tipo:this.tipoPregunta,
-    pregunta:this.pregunta,
-    opcion1:this.opcion1,
-    opcion2:this.opcion2,
-    opcion3:this.opcion3,
-    opcion4:this.opcion4
-     })
-    .map(res => res.json())
-    .subscribe((quote) =>{
-      console.info(quote);
-     });
+        this.cargando = true;
+        console.info(this.encuesta);
 
-      this.viewCtrl.dismiss();
-}
+        this.http.post("http://tppps2.hol.es/ws1/encuestas/alta", {
+            descripcion:this.encuesta.descripcion,
+            fechaInicio:this.encuesta.fechaInicio,
+            fechaFin:this.encuesta.fechaFin
+        })
+        .map(res => res.json())
+        .subscribe((quote) => {
+            console.info(quote);
+            this.cargando = false;
+            this.viewCtrl.dismiss(true);
+        }, e => {
+            this.mostrarMensaje('Hubo un error al guardar la encuesta. Intente de nuevo.');
+            this.cargando = false;
+        });
+
+    }
+
+    altaPregunta() {
+
+        this.cargando = true;
+
+        console.log(this.idEncuesta);
+
+        if(this.tipoPregunta =="1") {
+            this.tipoPregunta ="text";
+        } else if(this.tipoPregunta =="2") {
+            this.tipoPregunta ="radio";
+        } else if(this.tipoPregunta =="3") {
+            this.tipoPregunta ="checkbox";
+        }
+
+        this.http.post("http://tppps2.hol.es/ws1/pregunta/alta", {
+            idEncuesta:this.idEncuesta,
+            tipo:this.tipoPregunta,
+            pregunta:this.pregunta,
+            opcion1:this.opcion1,
+            opcion2:this.opcion2,
+            opcion3:this.opcion3,
+            opcion4:this.opcion4
+        })
+        .map(res => res.json())
+        .subscribe((quote) => {
+            this.cargando = false;
+            console.info(quote);
+            this.viewCtrl.dismiss(true);
+        }, e => {
+            this.mostrarMensaje('Hubo un error al guardar la pregunta. Intente de nuevo.');
+            this.cargando = false;
+        });
+
+    }
+
+    mostrarMensaje (mensaje) {
+        let toast = this.toastCtrl.create({
+            message: mensaje,
+            duration: 3000,
+            position: 'bottom'
+        });
+        toast.present();
+    }
 
 
 }
